8d6798b37cd9a7ef8db0c2a75d7749ee258b8c1f
John Ramsden <johnramsden@riseup.net>
2018-12-09 17:06:08 -0800

    Test calling libzfs_init()

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-
 include!(concat!(env!("OUT_DIR"),"/bindings.rs"));
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init() {
+        unsafe {
+            let lzc = libzfs_core_init();
+
+            let hdl = libzfs_init();
+
+            assert!(!hdl.is_null());
+        }
+    }
+
+}
